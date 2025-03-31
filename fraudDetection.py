f9c97169cc49fdb7e8ba198c461f0d930319e51a
Harshita Sanwal <119100652+HarshitaCoder@users.noreply.github.com>
2025-03-31 20:41:51 +0530

    fraudDetection.py

--- fraudDetection.py
+++ fraudDetection.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load the dataset
-file_path = '/mnt/data/creditcard.csv'
+file_path = 'C:\Users\Harshita Singh\Downloads\creditcard.csv'
 data = pd.read_csv(file_path)
 
 # Handle class imbalance if necessary (Oversampling/Undersampling)
